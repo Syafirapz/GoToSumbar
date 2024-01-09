6bbb208faa4354f913d3f69d0369bbbdf8f0723f
Syafirapz <100488786+Syafirapz@users.noreply.github.com>
2024-01-09 16:24:52 +0700

    Update darkModeActivity.java

--- app/src/main/java/com/app/gotosumbar/darkModeActivity.java
+++ app/src/main/java/com/app/gotosumbar/darkModeActivity.java
@@ -17,15 +17,21 @@ public class darkModeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dark_mode);
-
+        
+        // Inisialisasi RadioGroup dari layout
         radioGroup = findViewById(R.id.radio);
 
+         // Memberikan listener untuk menangani perubahan RadioButton yang dipilih
         radioGroup.setOnCheckedChangeListener((radioGroup, i) -> {
             RadioButton radioButton = findViewById(i);
             int id = radioButton.getId();
+
+             // Jika RadioButton "Dark" dipilih, aktifkan Mode Gelap dan tampilkan notifikasi
             if (id == R.id.dark){
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
                 Toast.makeText(this, "Dark Mode Aktif...", Toast.LENGTH_SHORT).show();
+                
+            // Jika RadioButton "Light" dipilih, aktifkan Mode Terang dan tampilkan notifikasi
             } else if (id == R.id.light) {
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
                 Toast.makeText(this, "Light Mode Aktif...", Toast.LENGTH_SHORT).show();
