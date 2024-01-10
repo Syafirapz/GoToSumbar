29f450c3f51fc64ae20dc2fdf666d2bdb916bd74
Syafirapz <100488786+Syafirapz@users.noreply.github.com>
2024-01-10 18:22:04 +0700

    Update FirebaseHandler.java

--- app/src/main/java/com/app/gotosumbar/FirebaseHandler.java
+++ app/src/main/java/com/app/gotosumbar/FirebaseHandler.java
@@ -1,13 +1,15 @@
 package com.app.gotosumbar;
 
 import android.app.Application;
-
+// Membuat kelas FirebaseHandler yang merupakan turunan dari kelas Application
 import com.google.firebase.database.FirebaseDatabase;
 
 public class FirebaseHandler extends Application {
+    // Override method onCreate() dari kelas Application
     @Override
     public void onCreate() {
-        super.onCreate();
+        super.onCreate(); // Memanggil metode onCreate() kelas induk
+         // Mengaktifkan mode persistensi untuk instance FirebaseDatabase
         FirebaseDatabase.getInstance().setPersistenceEnabled(true);
     }
 }
